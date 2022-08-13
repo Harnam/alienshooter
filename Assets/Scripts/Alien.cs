8d6f02390696a26301bd9518eea7a438162228d7
Harnam Singh <harnamsinghsandhu04@gmail.com>
2022-08-13 08:05:01 +0530

    alien move and platform change

--- Assets/Scripts/Alien.cs
+++ Assets/Scripts/Alien.cs
@@ -5,16 +5,23 @@ using UnityEngine;
 public class Alien : MonoBehaviour
 {
 
+    [SerializeField]
+    private float speed = 5f;
+    private Transform player;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        player = GameObject.FindWithTag("Player").GetComponent<Transform>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float step = speed * Time.deltaTime;
+
+        // move sprite towards the target location
+        transform.position = Vector2.MoveTowards(transform.position, new Vector2(player.position.x, player.position.y), step);
     }
 
     private void OnEnable()
